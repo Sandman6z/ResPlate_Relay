a98a836b31cf3104f66dd47054a60d2542bd735c
Sandman6z <zbn0238@163.com>
2024-06-04 04:13:00 +0800

    create project

---
+++ MDK-ARM/RTE/_ResPlate_Relay/RTE_Components.h
@@ -0,0 +1,15 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'ResPlate_Relay' 
+ * Target:  'ResPlate_Relay' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+
+#endif /* RTE_COMPONENTS_H */
